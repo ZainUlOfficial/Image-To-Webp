08e5da9903d9341dd1d74f61d6596d4e6ee9abc2
Zain Ul Arfeen <zainularfeen@live.com>
2020-09-02 04:33:44 +0500

    update js file png jpg convertion with webp

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+!function(e,n,A){function o(e,n){return typeof e===n}function t(){var e,n,A,t,a,i,l;for(var f in r)if(r.hasOwnProperty(f)){if(e=[],n=r[f],n.name&&(e.push(n.name.toLowerCase()),n.options&&n.options.aliases&&n.options.aliases.length))for(A=0;A<n.options.aliases.length;A++)e.push(n.options.aliases[A].toLowerCase());for(t=o(n.fn,"function")?n.fn():n.fn,a=0;a<e.length;a++)i=e[a],l=i.split("."),1===l.length?Modernizr[l[0]]=t:(!Modernizr[l[0]]||Modernizr[l[0]]instanceof Boolean||(Modernizr[l[0]]=new Boolean(Modernizr[l[0]])),Modernizr[l[0]][l[1]]=t),s.push((t?"":"no-")+l.join("-"))}}function a(e){var n=u.className,A=Modernizr._config.classPrefix||"";if(c&&(n=n.baseVal),Modernizr._config.enableJSClass){var o=new RegExp("(^|\\s)"+A+"no-js(\\s|$)");n=n.replace(o,"$1"+A+"js$2")}Modernizr._config.enableClasses&&(n+=" "+A+e.join(" "+A),c?u.className.baseVal=n:u.className=n)}function i(e,n){if("object"==typeof e)for(var A in e)f(e,A)&&i(A,e[A]);else{e=e.toLowerCase();var o=e.split("."),t=Modernizr[o[0]];if(2==o.length&&(t=t[o[1]]),"undefined"!=typeof t)return Modernizr;n="function"==typeof n?n():n,1==o.length?Modernizr[o[0]]=n:(!Modernizr[o[0]]||Modernizr[o[0]]instanceof Boolean||(Modernizr[o[0]]=new Boolean(Modernizr[o[0]])),Modernizr[o[0]][o[1]]=n),a([(n&&0!=n?"":"no-")+o.join("-")]),Modernizr._trigger(e,n)}return Modernizr}var s=[],r=[],l={_version:"3.6.0",_config:{classPrefix:"",enableClasses:!0,enableJSClass:!0,usePrefixes:!0},_q:[],on:function(e,n){var A=this;setTimeout(function(){n(A[e])},0)},addTest:function(e,n,A){r.push({name:e,fn:n,options:A})},addAsyncTest:function(e){r.push({name:null,fn:e})}},Modernizr=function(){};Modernizr.prototype=l,Modernizr=new Modernizr;var f,u=n.documentElement,c="svg"===u.nodeName.toLowerCase();!function(){var e={}.hasOwnProperty;f=o(e,"undefined")||o(e.call,"undefined")?function(e,n){return n in e&&o(e.constructor.prototype[n],"undefined")}:function(n,A){return e.call(n,A)}}(),l._l={},l.on=function(e,n){this._l[e]||(this._l[e]=[]),this._l[e].push(n),Modernizr.hasOwnProperty(e)&&setTimeout(function(){Modernizr._trigger(e,Modernizr[e])},0)},l._trigger=function(e,n){if(this._l[e]){var A=this._l[e];setTimeout(function(){var e,o;for(e=0;e<A.length;e++)(o=A[e])(n)},0),delete this._l[e]}},Modernizr._q.push(function(){l.addTest=i}),Modernizr.addAsyncTest(function(){function e(e,n,A){function o(n){var o=n&&"load"===n.type?1==t.width:!1,a="webp"===e;i(e,a&&o?new Boolean(o):o),A&&A(n)}var t=new Image;t.onerror=o,t.onload=o,t.src=n}var n=[{uri:"data:image/webp;base64,UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoBAAEAAwA0JaQAA3AA/vuUAAA=",name:"webp"},{uri:"data:image/webp;base64,UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAwAAAABBxAR/Q9ERP8DAABWUDggGAAAADABAJ0BKgEAAQADADQlpAADcAD++/1QAA==",name:"webp.alpha"},{uri:"data:image/webp;base64,UklGRlIAAABXRUJQVlA4WAoAAAASAAAAAAAAAAAAQU5JTQYAAAD/////AABBTk1GJgAAAAAAAAAAAAAAAAAAAGQAAABWUDhMDQAAAC8AAAAQBxAREYiI/gcA",name:"webp.animation"},{uri:"data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=",name:"webp.lossless"}],A=n.shift();e(A.name,A.uri,function(A){if(A&&"load"===A.type)for(var o=0;o<n.length;o++)e(n[o].name,n[o].uri)})}),t(),a(s),delete l.addTest,delete l.addAsyncTest;for(var p=0;p<Modernizr._q.length;p++)Modernizr._q[p]();e.Modernizr=Modernizr}(window,document);
 /*!
  * jQuery JavaScript Library v2.1.1
  * http://jquery.com/
@@ -3009,6 +3010,7 @@ function FastClick(a,b){"use strict";function c(a,b){return function(){return a.
 
       obj = new Backstretch(this, images, options);
       $this.data('backstretch', obj);
+
     });
   };
 
@@ -3077,9 +3079,15 @@ function FastClick(a,b){"use strict";function c(a,b){return function(){return a.
      */
     this.images = $.isArray(images) ? images : [images];
 
+    // var fileName,extension, img;
+
     // Preload images
     $.each(this.images, function () {
       $('<img />')[0].src = this;
+      // extension = this.substr( (this.lastIndexOf('.') +1) );
+      // fileName = this.split('.').slice(0, -1).join('.') + '.webp';
+      // $('<img />').parents('picture').prepend('<source srcset="' + fileName + '" data-srcset="' + fileName + '" type="image/webp"><source srcset="' + this.src + '" data-srcset="' + this.src + '" type="image/'+ extension+'">');
+      // console.log();
     });    
 
     // Convenience reference to know if the container is body.
@@ -3120,13 +3128,10 @@ function FastClick(a,b){"use strict";function c(a,b){return function(){return a.
       position: this.isBody && supportsFixedPosition ? 'fixed' : 'absolute'
     });
 
-    // this.wrap('<picture></picture>');
-    
 
     // Set the first image
     this.index = 0;
     this.show(this.index);
-
 
     // Listen for resize
     $(window).on('resize.backstretch', $.proxy(this.resize, this))
@@ -3218,9 +3223,14 @@ function FastClick(a,b){"use strict";function c(a,b){return function(){return a.
                         // Show the image, then delete the old one
                         // "speed" option has been deprecated, but we want backwards compatibilty
                         $(this).fadeIn(self.options.speed || self.options.fade, function () {
-                          oldImage.remove();
+                          
+                        if (oldImage.parent().is( "picture" )) {
+                          	oldImage.parent().remove();
+                        }
 
-      					// $(this).wrap('<picture></picture>');
+                        oldImage.remove();
+
+                      	// console.log(oldImage);
 
 
 
@@ -3240,10 +3250,30 @@ function FastClick(a,b){"use strict";function c(a,b){return function(){return a.
                         // Resize
                         self.resize();
                       })
-                      .appendTo(self.$wrap);
+                      .appendTo(self.$wrap).wrap('<picture></picture>');
+
+                      // console.log(self.images[newIndex]);
+
+    	var fileName,extension, img;
+
+      extension = self.images[newIndex].substr( (self.images[newIndex].lastIndexOf('.') +1) );
+
+      fileName = self.images[newIndex].split('.').slice(0, -1).join('.') + '.webp';
+
+      
+
+      self.$img.parents('picture').prepend('<source srcset="' + fileName + '" data-srcset="' + fileName + '" type="image/webp"><source srcset="' + self.images[newIndex] + '" data-srcset="' + self.images[newIndex] + '" type="image/'+ extension+'">');
 
         // Hack for IE img onload event
-        self.$img.attr('src', self.images[newIndex]);
+
+		Modernizr.on('webp', function(result) {
+		  if (result) {
+        	self.$img.attr('src', fileName);
+		  }else{
+        	self.$img.attr('src', self.images[newIndex]);
+
+		  }
+		});
 
         return self;
       }
